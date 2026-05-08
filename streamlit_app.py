07183693edf13f13190813925c1f1f76e6a95e8c
Zoriak <168212978+Zoriak-DK@users.noreply.github.com>
2026-05-08 08:29:17 +0000

    V1.0.0 du projet streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Bienvenue sur le site de Zoriak ❤️")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Vous trouverez ici des outils et des liens vers des ressources utiles."
 )
+st.divider()
+st.header("Liens utiles")
+st.link_button("GitHub du projet", "https://github.com/Zoriak-DK/Zoriak_WebSite")
+st.link_button("OpenStreetMap", "https://www.openstreetmap.org/")
+st.link_button("Documentation de Streamlit", "https://docs.streamlit.io/")
+st.divider()
+st.header("Outils")
+st.write("Voici quelques outils que vous pouvez utiliser :")
+st.button("Outil 1")
+st.button("Outil 2")
+st.button("Outil 3")
